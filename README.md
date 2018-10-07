fb41d6fbf79c738649c5afd6bdd7c91cec4ac00c
pim <pim.sauter@gmail.com>
2018-10-07 23:01:58 +0200

    Final version, added README

---
+++ README.md
@@ -0,0 +1,19 @@
+# The hungry neighbourhood map
+This application shows you the best places to eat in a neighbourhood of your choosing.
+
+## Usage
+- Download this repo using `git clone`
+- Open `index.html` with your favourite web browser
+- Enter what you want to eat (e.g. 'Pizza')
+- Enter the place where you want to eat it (e.g. 'Rotterdam')
+- You can filter on Google Maps rating
+- The app will show relevant restaurants on the map.
+- Click on a marker to get more info, like the rating, Streetview image and weather
+
+## Libraries and APIs used
+- KnockoutJS
+- jQuery
+- Material design
+
+- Google Maps API
+- Openweathermap API
--- js/app.js
+++ js/app.js
@@ -1,87 +1,159 @@
-var Place = function(result, map) {
-    that = this;
-
-    that.name = ko.observable(result.name);
-    that.coordinates = ko.observable(result.geometry.location);
-    
-    that.makeMarker = function () {
-        marker = new google.maps.Marker({
-            map: map,
-            position: that.coordinates(),
-            title: that.name()
-        });
-
-        return marker;
-    }
-
-    that.marker = that.makeMarker();
-
-    that.marker.addPanorama = function () {
-        var panorama = new google.maps.StreetViewPanorama(
-          document.getElementById('panorama'), {
-            position: this.position,
-            pov: {
-              heading: 34,
-              pitch: 10
-            }
-          });
-        map.setStreetView(panorama);
-    }
-
-    that.marker.showInfoWindow = function () {
-        map.infoWindow.setContent('<div> <p>' + this.title + '</p> </div> <div id="panorama"></div>')
-        map.infoWindow.open(map, this);
-        this.addPanorama();
-    }
-
-    that.showInfoWindow = that.marker.showInfoWindow.bind(that.marker)
-
-    google.maps.event.addListener(that.marker, 'click', function () {
-        this.showInfoWindow();
-    });
-}
-
 var ViewModel = function () {
-    var that = this;
+    const that = this;
        
-    // Initialize the map
-    that.map = new google.maps.Map(document.getElementById('map'), {
+    that.init = function () {
+
+        // Initialize the map
+        that.map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 51.919662988, lng: 4.4753314},
-            zoom: 13
-        });
+            zoom: 15,
+            mapTypeControlOptions: {position: google.maps.ControlPosition.TOP_CENTER}
+        });        
+        
+        // Assign DOM elements to KO observables
+        that.markers = ko.observableArray();
+        that.errorMessage = ko.observable();
+        that.query = ko.observable("Pizza");
+        that.placeQuery = ko.observable();
+        that.bounds = ko.observable(that.map.getBounds());
+        that.weather = ko.observable();
+        that.minRating = ko.observable(3);
+        that.menuVisible = ko.observable(true);
 
-    // Make placesService for looking up places from the Google Maps places API
-    that.placesService = new google.maps.places.PlacesService(that.map);
+        // Filter markers and list based on rating filter
+        that.filteredMarkers = ko.computed(function() {
+            if(!that.minRating()) {
+                that.showMarkers(that.markers())
+                return that.markers(); 
+            } else {
+                that.clearMarkers(that.markers());
+                return ko.utils.arrayFilter(that.markers(), function(marker) {
+                     if (marker.rating >= that.minRating()) {
+                        that.showMarker(marker);
+                        return true;
+                     };                     
+                });
+            }
+        }, that);
+       
+        // Service to return info for a search term
+        that.placesService = new google.maps.places.PlacesService(that.map);
+        
+        // Service to get a streetview panorama
+        that.streetViewService = new google.maps.StreetViewService();
 
-    // Make StreetViewService for getting street view images for a place
-    that.streetViewService = new google.maps.StreetViewService();
+        // Service to encode addresses to GPS coordinates
+        that.geocoder = new google.maps.Geocoder();
+        
+        // Window to display information for a marker
+        that.infoWindow = new google.maps.InfoWindow({maxHeight: 400});
 
-    that.places = ko.observableArray();
-    that.errorMessage = ko.observable();
-    that.map.infoWindow = new google.maps.InfoWindow();
-
-    // Keep track of what the user queried
-    that.query = ko.observable("Pizza");
+        // Searchbox for current location
+        that.placeInput = document.getElementById("placeInput")
+        that.searchBox = new google.maps.places.SearchBox(that.placeInput);
+    }
 
     // Clear markers for all places on the map
-    that.clearMarkers = function() {
-        for (let place of that.places()) {
-            place.marker.setMap(null);
+    that.clearMarkers = function(markers) {
+        for (let marker of markers) {
+            marker.setMap(null);
         }
     }
 
-    that.callback = function (results, status) {
-        // Clear the markers off the map
-        that.clearMarkers();
+    // Show a set of markers on the map
+    that.showMarkers = function(markers) {
+        for (let marker of markers) {
+            marker.setMap(that.map);
+        };
+    }
 
-        // Empty the places array
-        that.places([]);
+    // Show one marker on the map
+    that.showMarker = function (marker) {
+        marker.setMap(that.map);
+    }
+
+    that.makeMarkerBounce = function (marker) {
+        // Stop the bouncing of other markers
+        that.stopMarkersBouncing(that.markers())
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+
+    that.stopMarkersBouncing = function (markers) {
+        for (let marker of markers) {
+            marker.setAnimation(null);
+        }
+    }
+
+    // Get the weather for the current map location from the openweathermap API
+    that.getWeather = function () {
+
+        let mapCenter = that.map.getCenter()
+
+        let url = `http://api.openweathermap.org/data/2.5/weather?lat=${mapCenter.lat()}&lon=${mapCenter.lng()}&APPID=947fb8237f2dbc03a4fe2ea0a1feab24`
+        
+        function updateWeather(data, status) {
+            if (status == "success") {
+                that.weather(`Weather: ${data.weather[0].description}`)
+            }
+            else {
+                that.weather(`Weather unavailable`)   
+            }
+        }
+
+        $.get(url, function(data, status){
+            updateWeather(data, status);
+        });
+    }
+
+    // Add a StreetviewPanorama to a marker
+   that.addPanorama = function (marker) {
+        const panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('panorama'), {
+            position: marker.position,
+            pov: {
+              heading: 0,
+              pitch: 10
+            }
+          });
+
+        that.map.setStreetView(panorama);
+    }
+
+    // Show infowindow on marker, displaying name, rating, streetview and weather
+    that.showInfoWindow = function (marker) {
+        that.infoWindow.setContent(
+            `<div id ="title">  ${marker.title} | ${marker.rating} &#9733</div>` + 
+            `<div id="panorama">No panorama found</div>` +
+            `<div id="weather"> ${that.weather()} </div>` +
+            ` <p class="footnote">(Data by <a href="https://openweathermap.org/api">Openweathermap)</a></p>` 
+            )
+
+        that.infoWindow.open(map, marker);
+        that.addPanorama(marker);
+    }
+
+    // Make markers according to search query
+    that.makeMarkers = function (results, status) {
+        // Empty the markers array
+        that.markers([]);
 
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             that.errorMessage('');
 
-            for (result of results) {
-                that.places.push(new Place(result, that.map));
+            for (let result of results) {
+                let marker = new google.maps.Marker({
+                    map: null,
+                    position: result.geometry.location,
+                    title: result.name,
+                    rating: result.rating
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    that.showInfoWindow(this);
+                    that.makeMarkerBounce(this);
+                });     
+
+                that.markers.push(marker);
             };
         } 
 
@@ -92,17 +164,36 @@ var ViewModel = function () {
     }
 
     // Search for places
-    that.search = function () {
+    that.searchPlaces = function () {
+        let places = that.searchBox.getPlaces();
+        
+        if (!places) {
+            that.errorMessage('Please enter a valid location')
+            return
+        }
+        else {
+            let place = places[0].geometry.location;
+            that.map.setCenter(place);
+        }
+        that.getWeather();
+                
         that.placesService.nearbySearch({
                   bounds: that.map.getBounds(),
                   radius: 1000,
                   type: ['restaurant'],
                   keyword: that.query()
-                }, that.callback);
+                }, that.makeMarkers);
+    }
+
+    // Toggle side menu
+    that.toggleMenu = function () {
+        that.menuVisible(!that.menuVisible())
     }
 };
 
+function initView () {
+    viewModel = new ViewModel();
+    viewModel.init();
 
-function initMap () {
-    ko.applyBindings(new ViewModel());
+    ko.applyBindings(viewModel);
 }
